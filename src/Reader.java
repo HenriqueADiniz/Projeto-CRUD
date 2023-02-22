a954f55897961adab3a83f9218be0cb9ef2d468b
VianaSamuel <samuluizzz@hotmail.com>
2023-02-22 18:27:45 -0300

    Adição de comentários.

--- src/Reader.java
+++ src/Reader.java
@@ -1,21 +1,29 @@
-import java.io.FileReader;
-import com.opencsv.CSVReader;
 import java.util.List;
 import java.util.Arrays;
 import java.util.ArrayList;
+import java.io.FileReader;
+import com.opencsv.CSVReader;
 
 public class Reader {
+    // caminho do arquivo .csv
     private static final String CSV_PATH = "/tmp/pokemons.csv";
 
+    /*
+     * inicializa o leitor
+     * inicializa LISTA de lista de strings (lista de strings = colunas, sao multivaloradas por causa dos campos)
+     * pula a primeira coluna com o nome de cada campo
+     * adiciona colunas a lista
+     */
     public static void main(String[] args) {
         try {
             CSVReader reader = new CSVReader(new FileReader(CSV_PATH));
 
             List<List<String>> linhas = new ArrayList<List<String>>();
-            String[] colunas = null;
+            String[] coluna = null;
 
-            while ((colunas = reader.readNext()) != null){
-                linhas.add(Arrays.asList(colunas));
+            coluna = reader.readNext();
+            while ((coluna = reader.readNext()) != null){
+                linhas.add(Arrays.asList(coluna));
             }
 
             linhas.forEach(cols -> {
