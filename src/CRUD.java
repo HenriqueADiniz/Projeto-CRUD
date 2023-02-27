4dfce3d8b272390b2dad54d6694cefea3d67eea5
Henrique <henriquealdiniz@gmail.com>
2023-02-26 21:40:38 -0300

    Update
    Metodos:
    Sets e Gets
    toByteArray
    fromByteArray
    Create
    Delete

--- src/CRUD.java
+++ src/CRUD.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.io.RandomAccessFile;
 
 public class CRUD {
     public static void main(String[] args) {
@@ -61,4 +62,61 @@ public class CRUD {
             Thread.currentThread().interrupt();
         }
     }
+        // --------------- Atributos ---------------
+       static int ultimoId;
+        // --------------- Create ---------------
+        public static boolean create(RandomAccessFile raf, Pokemon pokemon) { // Cria um pokemon
+
+            try {
+                raf.seek(raf.length()); // Posiciona o ponteiro no final do arquivo
+                raf.writeByte(0); // Escreve a lapide (0 = ativo)
+                raf.writeInt(pokemon.toByteArray().length); // Escreve o tamanho do registro
+                raf.writeInt(pokemon.getNumber()); // Escreve o numero do pokemon
+                raf.writeInt(pokemon.getHP()); // Escreve o Hp do pokemon
+                raf.writeInt(pokemon.getAtt()); // Escreve o Att do pokemon
+                raf.writeInt(pokemon.getDef()); // Escreve a Def do pokemon
+                raf.writeUTF(pokemon.getName()); // Escreve o nome do pokemon
+                raf.writeUTF(pokemon.getType1()); // Escreve o Type1 do pokemon
+                raf.writeUTF(pokemon.getType2()); // Escreve o Type 2 do pokemon
+                // Escreve as abilities do pokemon
+                // Escreve a data do pokemon
+             
+    
+                raf.seek(0); // Posiciona o ponteiro no inicio do arquivo
+                raf.writeInt(ultimoId); // Atualiza o ultimo id
+    
+                return true;
+            } catch (Exception e) {
+                System.out.println("-> Erro ao criar registro!");
+                return false;
+            }
+        }
+        // --------------- Delete ---------------
+
+    public static boolean delete(RandomAccessFile raf, Pokemon pokemon) { // Exclui um Pokemon
+        try {
+            raf.seek(4); // Posiciona o ponteiro no inicio do arquivo
+            while(raf.getFilePointer() < raf.length()) { // Enquanto o ponteiro nao chegar no final do arquivo
+                if(raf.readByte() == 0) { // Se a lapide for 0 (ativo)
+                    int tam = raf.readInt();
+                    int id = raf.readInt();
+
+                    if(id == pokemon.getNumber()) { // Se o numero do pokemon for igual ao numero do  a ser excluido
+                        raf.seek(raf.getFilePointer() - 9); // Volta o ponteiro para o inicio do registro
+                        raf.writeByte(1); // Escreve a lapide 1 (excluido)
+                        return true;
+                    }else {
+                        raf.skipBytes(tam - 4); // Pula o resto do registro
+                    }
+                }else {
+                    raf.skipBytes(raf.readInt()); // Pula o registro
+                }
+            }
+            return false;
+        }catch (Exception e) {
+            System.out.println("-> Erro ao deletar registro!");
+            return false;
+        }
+    }
+
 }
--- src/Pokemon.java
+++ src/Pokemon.java
@@ -18,4 +18,53 @@ public class Pokemon {
         this.def = def;
         this.date = date;
     }
+        // --------------- Getters e Setters ---------------
+
+        // Numero da Pokedex
+        public int getnumber(){
+            return number;
+        }
+        public void setNumber(int number){
+            this.number = number;
+        }
+        // HP
+        public int getHP(){
+            return hp;
+        }
+        public void setHP(int hp){
+            this.hp = hp;
+        }
+        // Att
+        public int getAtt(){
+            return att;
+        }
+        public void setAtt(int att){
+            this.att = att;
+        }
+        // Def
+        public int getDef(){
+            return def;
+        }
+        public void setDef(int def){
+            this.def = def;
+        }
+        // Nome do Pokemon
+        public String getName() {return name;}
+        public void setName(String name) {this.name = name;}
+        // Type 1
+        public String getType1() {return type1;}
+        public void setType1(String type1) {this.type1 = type1;}
+        //Type 2
+        public String getType2() {return type2;}
+        public void setType2(String type2) {this.type2 = type2;}
+        // abilities
+        public String[] getAbilities() {return abilities;}
+        public void setAbilities(String[] abilities) {this.abilities = abilities;}
+        // Date
+        public Date getDate(){return date;}
+        public void setDate(Date date){this.date=date;}
+        
+
+
+
 }
